5974a2b221fc87439dc3c347db63c94152e158c3
곽경희 <82927462+kwakkyunghee@users.noreply.github.com>
2022-12-03 07:29:58 +0900

    카테고리 어느정도 완성, 상품 링크 진행 중

---
+++ index.js
@@ -0,0 +1,29 @@
+function toggleBtn1() {
+
+	  
+
+    // 토글 할 버튼 선택 (btn1)
+
+    const btn1 = document.getElementById('btn1');
+
+    
+
+    // btn1 숨기기 (display: none)
+
+    if(btn1.style.display !== 'none') {
+
+      btn1.style.display = 'none';
+
+    }
+
+    // btn` 보이기 (display: block)
+
+    else {
+
+      btn1.style.display = 'block';
+
+    }
+
+    
+
+  }
--- login.php
+++ login.php
@@ -3,3 +3,26 @@
 
 
 ?>
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+</head>
+<body>
+    <form>
+        <div class="title">-->
+            <!-- 로고 이미지 -->
+           <!-- <p id="header">
+                <img src="seoulstore_images/ss_logo_gnb.jpg" width="130" height="20">
+                <button type="submit" class="login">로그인</button>
+                
+                <button type="submit" class="sign_up">회원가입</button>
+            </p>
+        </div>
+    </form>
+</body>
+</html> -->
