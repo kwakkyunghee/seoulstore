a915d672b487656e3b5b9422302a8239a148d658
곽경희 <82927462+kwakkyunghee@users.noreply.github.com>
2022-12-05 05:52:16 +0900

    html, css

--- login.php
+++ login.php
@@ -4,7 +4,7 @@
 
 ?>
 
-<!-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -14,15 +14,25 @@
 </head>
 <body>
     <form>
-        <div class="title">-->
-            <!-- 로고 이미지 -->
-           <!-- <p id="header">
-                <img src="seoulstore_images/ss_logo_gnb.jpg" width="130" height="20">
-                <button type="submit" class="login">로그인</button>
-                
-                <button type="submit" class="sign_up">회원가입</button>
-            </p>
-        </div>
+        <header>
+            <div class="title">
+                <!-- 로고 이미지 -->
+                <p id="header">
+                    <img src="seoulstore_images/ss_logo_gnb.jpg" width="130" height="20">
+                    <p id="and">
+                        <button type="submit" class="login">로그인</button>
+                        <button type="submit" class="sign_up">회원가입</button>
+                    </p>
+                </p>
+            </div>
+            <div>
+            <table>
+                <tr><td>아이디</td><td ><input type="text" name="uid"></td></tr>
+                <tr><td>비밀번호</td><td ><input type="password" name="upass"></td></tr>
+            </table>
+            <button type="submit" class="btn btn-primary">로그인</button>
+            </div>
+        </header>
     </form>
 </body>
-</html> -->
+</html>
